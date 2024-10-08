ce3032679fc62bb65b07b37a102249ca7ac8d452
Yunxi-Zhou <161828903+Yunxi-Zhou@users.noreply.github.com>
2024-10-08 06:59:28 -0400

    commit passive buzzer

---
+++ include/passive_buzzer.h
@@ -0,0 +1,7 @@
+#ifndef PASSIVE_BUZZER_H
+#define PASSIVE_BUZZER_H
+
+void set_passive();
+void loop_passive();
+
+#endif
---
+++ src/passive_buzzer.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#include "pitches.h"
+#include "passive_buzzer.h"
+
+// notes in melody
+int melody[] = {
+    NOTE_C5, NOTE_D5,NOTE_E5,NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6};
+int duration = 500; // 500 ms
+
+void set_passive() {
+    Serial.begin(9600);
+}
+
+void loop_passive() {
+    for (int thisNote = 0; thisNote < 8; thisNote++) {
+        // pin8 output the voice, every scale is 0.5 second
+        tone(8, melody[thisNote], duration);
+
+        // output the voice after several minutes
+        delay(1000);
+        Serial.print("melody at duration: ");
+        Serial.println(melody[thisNote]);
+    }
+
+    //restart after two seconds
+    delay(2000);
+}
